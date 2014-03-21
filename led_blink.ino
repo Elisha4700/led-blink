e7d689df36240874df6ff9822c2663c623beded3
Elisha Shapiro <elishas@sinapsa.co.il>
2014-03-21 10:57:09 +0200

    Blinking onboard LED every X seconds

--- led_blink.ino
+++ led_blink.ino
@@ -1,1 +1,24 @@
+/**
+LED blink
+Blinks the onboard LED every X seconds
+*/
 
+
+int led_pin = 13;
+int SEC = 1000;
+int blink_rate = 1 * SEC;
+
+void setup() 
+{
+  // intialize pins & outputs
+  pinMode(led_pin, OUTPUT);
+  
+}
+  
+void loop() 
+{
+  digitalWrite(led_pin, HIGH);
+  delay(blink_rate);
+  digitalWrite(led_pin, LOW);
+  delay(blink_rate);
+}
